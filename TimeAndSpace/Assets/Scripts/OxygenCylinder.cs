7c7a72f0cc6613c4e354cd8ae4b952aa7ea33bc2
RitaTheDeveloper <ritagrashchenkova@gmail.com>
2023-11-04 23:46:40 +0300

    add oxygenCylinder

---
+++ TimeAndSpace/Assets/Scripts/OxygenCylinder.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OxygenCylinder : MonoBehaviour
+{
+    [SerializeField] private float _amountOfOxygen;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Spaceship")
+        {
+            other.gameObject.GetComponent<OxygenController>().AddOxygen(_amountOfOxygen);
+
+            Destroy(gameObject);
+        }
+    }
+
+}
