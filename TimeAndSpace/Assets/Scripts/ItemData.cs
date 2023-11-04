8fd06e8638bfb7e3a690cc63567ddbb22d78269e
IvanLindgren <denis.kornilov12@yandex.ru>
2023-11-04 23:49:55 +0300

    itemdata

---
+++ TimeAndSpace/Assets/Scripts/ItemData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "ItemData")]
+public class ItemData : ScriptableObject
+{
+    public int planetIndex;
+    public Sprite planetImage;
+    public GameObject planetPrefab;
+
+}
