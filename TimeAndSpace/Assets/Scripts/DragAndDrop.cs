6bf850efeca1e7d10c8419f7019bf06a3fe7a7a1
IvanLindgren <denis.kornilov12@yandex.ru>
2023-11-04 18:31:50 +0300

    script_drag

---
+++ TimeAndSpace/Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDropUI : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
+{
+    Vector3 offset;
+
+    void Awake()
+    {
+       
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition + offset;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        offset = transform.position - Input.mousePosition;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        
+    }
+}
