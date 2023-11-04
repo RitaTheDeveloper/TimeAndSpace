359c4ab23069b49c316c081989b035cd4498023a
RitaTheDeveloper <ritagrashchenkova@gmail.com>
2023-11-04 16:31:11 +0300

    third commit
    Гравитация
    Стартовая скорость корабля
    Астероид
    Конечная точка (планета земля)
    Условия победы и поражения

---
+++ TimeAndSpace/Assets/Scripts/HomePlanet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomePlanet : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Spaceship")
+        {
+            GameManager.instance.Win();
+        }
+    }
+}
