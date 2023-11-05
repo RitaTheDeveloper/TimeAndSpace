95b0fd94fbd1bc2a5de34b26bf6f6eb626e17596
IvanLindgren <denis.kornilov12@yandex.ru>
2023-11-06 00:39:23 +0300

    drag

---
+++ TimeAndSpace/Assets/Scripts/DragDropPlanet2.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragDropPlanet : MonoBehaviour
+{
+
+    private Vector3 mousePosition;
+    private float mass;
+
+    private Vector3 getMousePosition()
+    {
+        return Camera.main.WorldToScreenPoint(transform.position);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mass = GetComponent<Rigidbody>().mass;
+    }
+
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        GetComponent<Rigidbody>().mass = mass;
+    }
+    
+    
+    private void OnMouseDown()
+    {
+        GetComponent<Rigidbody>().mass = 0;
+        mousePosition = Input.mousePosition - getMousePosition();
+        
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition - mousePosition);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
